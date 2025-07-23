fff8a6aaa8401d5cab9e8f784041f61da79305a9
alidon20232023 <alidon20232023@gmail.com>
2025-07-24 00:38:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// alert("Hey welcome to Netflix");
+
+// console.log("Welcome to our resouces and start watching videos!");
+// console.log("Welcome to our resouces and start watching videos!");
+// console.log("Welcome to our resouces and start watching videos!");
+ 
+// var a = prompt("Enter your number?");
+
+// console.log("Your number is " + a);
+
+
